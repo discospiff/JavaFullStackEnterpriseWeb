c26d71cbb542cb45dc1ab72c1c5e5d88121d504b
Brandan Jones <brandan.jones@uc.edu>
2015-09-09 23:05:48 -0400

    Using EclEmma for code covereage.  Created additional UI tests.

---
+++ PlantPlaces/src/com/plantplaces/ui/test/TestSearchPlants.java
@@ -0,0 +1,74 @@
+package com.plantplaces.ui.test;
+
+import org.junit.Test;
+
+import com.plantplaces.dto.Plant;
+import com.plantplaces.ui.SearchPlants;
+
+import junit.framework.TestCase;
+
+public class TestSearchPlants extends TestCase {
+
+	private SearchPlants searchPlants;
+	private String execute;
+
+
+	@Test
+	public void testSearchPlantsExecute() {
+		giveSearchPlantsCreatedWithRedbud();
+		whenInvokeExecute();
+		thenVerifyReturnSuccess();
+	}
+
+	@Test
+	public void testSearchPlantsNoRedbud() {
+		givenSearchPlantsCreatedWithoutRedbud();
+		whenInvokeExecute();
+		thenVerifyReturnNoResults();
+	}
+	
+	@Test
+	public void testSearchPlantsNull() {
+		givenSearchPlantsCreatdWithNullPlant();
+		whenInvokeExecute();
+		thenVerifyReturnNoResults();
+	}
+	
+	private void givenSearchPlantsCreatdWithNullPlant() {
+		searchPlants = new SearchPlants();
+		
+	}
+
+	private void thenVerifyReturnNoResults() {
+		// TODO Auto-generated method stub
+		assertEquals("noresults", execute);
+	}
+
+	private void givenSearchPlantsCreatedWithoutRedbud() {
+		// TODO Auto-generated method stub
+		searchPlants = new SearchPlants();
+		Plant plant = new Plant();
+		plant.setName("Pawpaw");
+		searchPlants.setPlant(plant);
+	}
+
+	private void thenVerifyReturnSuccess() {
+		assertEquals("success", execute);
+		
+	}
+
+
+	private void whenInvokeExecute() {
+		execute = searchPlants.execute();
+		
+	}
+
+
+	private void giveSearchPlantsCreatedWithRedbud() {
+		searchPlants = new SearchPlants();
+		Plant plant = new Plant();
+		plant.setName("Redbud");
+		searchPlants.setPlant(plant);
+	}
+	
+}
